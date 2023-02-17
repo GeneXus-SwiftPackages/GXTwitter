6c5e7d94c3a5aadb610766064cbc907c0facd2dd
Build Er <builder@macmini6.local>
2023-02-16 23:52:24 -0300

    Package version: 1.0.0-beta+20230217024255

---
+++ Package.swift
@@ -0,0 +1,28 @@
+// swift-tools-version: 5.7
+import PackageDescription
+
+let package = Package(
+	name: "GXTwitter",
+	platforms: [.iOS("12.0")],
+	products: [
+		.library(
+			name: "GXTwitter",
+			targets: ["GXTwitterWrapper"])
+	],
+	dependencies: [
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", branch: "beta")
+	],
+	targets: [
+		.target(name: "GXTwitterWrapper",
+				dependencies: [
+					"GXTwitter",
+					.product(name: "GXCoreUI", package: "GXCoreUI", condition: .when(platforms: [.iOS]))
+				],
+				path: "Sources"),
+		.binaryTarget(
+			name: "GXTwitter",
+			url: "https://pkgs.genexus.dev/iOS/beta/GXTwitter-1.0.0-beta+20230217024255.xcframework.zip",
+			checksum: "7faf86a901a57f510fb5fc348255cb68679177c1566d707f17412e2f7226c1e8"
+		)
+	]
+)
