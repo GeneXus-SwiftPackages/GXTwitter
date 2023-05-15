1a676f902df9029476e4dbf7d71501217baa6ed8
genexusbot <builder@genexus.com>
2023-05-15 14:55:04 -0300

    Package version: 1.0.0-beta+20230515174624

--- Package.swift
+++ Package.swift
@@ -21,8 +21,8 @@ let package = Package(
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXTwitter",
-			url: "https://pkgs.genexus.dev/iOS/beta/GXTwitter-1.0.0-beta+20230515153434.xcframework.zip",
-			checksum: "31cbed2cff3898a9f5db6e262d10595062588953b0b3133583e79d0918006091"
+			url: "https://pkgs.genexus.dev/iOS/beta/GXTwitter-1.0.0-beta+20230515174624.xcframework.zip",
+			checksum: "94df6c1c0404521c7f0ab5a7963e8f3e48d92c96e6dd1011dc0016a3819aad99"
 		)
 	]
 )
