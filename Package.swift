6a15646119e19b3a61547f63564294383a58f10f
Build Er <builder@macmini6.local>
2023-05-16 14:57:04 -0300

    Package version: 1.0.0-beta+20230516174834

--- Package.swift
+++ Package.swift
@@ -21,8 +21,8 @@ let package = Package(
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXTwitter",
-			url: "https://pkgs.genexus.dev/iOS/beta/GXTwitter-1.0.0-beta+20230515174624.xcframework.zip",
-			checksum: "94df6c1c0404521c7f0ab5a7963e8f3e48d92c96e6dd1011dc0016a3819aad99"
+			url: "https://pkgs.genexus.dev/iOS/beta/GXTwitter-1.0.0-beta+20230516174834.xcframework.zip",
+			checksum: "858ea3475d2347438698dbf66606e726a53893dfa51d7d4b1b88a6b0383e6b65"
 		)
 	]
 )
