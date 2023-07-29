ce830808d53f94569296f45c2a0064cffb3bb03a
genexusbot <builder@genexus.com>
2023-07-29 18:42:26 -0300

    Package version: 1.1.0-rc.1

---
+++ Package.swift
@@ -0,0 +1,28 @@
+// swift-tools-version: 5.7
+import PackageDescription
+
+let package = Package(
+	name: "GXTwitter",
+	platforms: [.iOS("12.0")],
+	products: [
+		.library(
+			name: "GXTwitter",
+			targets: ["GXTwitterWrapper"])
+	],
+	dependencies: [
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", exact: "1.1.0-rc.1")
+	],
+	targets: [
+		.target(name: "GXTwitterWrapper",
+				dependencies: [
+					"GXTwitter",
+					.product(name: "GXCoreUI", package: "GXCoreUI", condition: .when(platforms: [.iOS]))
+				],
+				path: "Sources"),
+		.binaryTarget(
+			name: "GXTwitter",
+			url: "https://pkgs.genexus.dev/iOS/releases/GXTwitter-1.1.0-rc.1.xcframework.zip",
+			checksum: "f3ddd4113b94200c83f80e0167d2258872a0e51604645eb2d9078dfa0335008d"
+		)
+	]
+)
