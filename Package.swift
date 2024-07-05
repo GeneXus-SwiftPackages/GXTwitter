5aab53f1a0d1835dc980d2a221ca90a60d3da52d
Build Er <builder@macmini6.local>
2024-07-05 14:26:44 -0300

    Package version: 2.1.0-beta.26

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
 			targets: ["GXTwitterWrapper"])
 	],
 	dependencies: [
-		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", exact: "2.1.0-beta.25")
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", exact: "2.1.0-beta.26")
 	],
 	targets: [
 		.target(name: "GXTwitterWrapper",
@@ -21,8 +21,8 @@ let package = Package(
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXTwitter",
-			url: "https://pkgs.genexus.dev/iOS/beta/GXTwitter-2.1.0-beta.25.xcframework.zip",
-			checksum: "192d7cb85852733a9a0724205fb8e8883b617e53e38dec73d8f4034e854707ef"
+			url: "https://pkgs.genexus.dev/iOS/beta/GXTwitter-2.1.0-beta.26.xcframework.zip",
+			checksum: "2ee0a06cc4daffad984d17ba4ac84c5cd242b0fc5fe169c051d282a878529664"
 		)
 	]
 )
