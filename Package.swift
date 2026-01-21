621560a6de4d0f67c2faa9ee79f2cc988829e26c
The iOS Team @ GeneXus <builder@genexus.com>
2026-01-21 04:38:19 -0500

    Package version: 4.1.0-beta.21

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
 			targets: ["GXTwitterWrapper"])
 	],
 	dependencies: [
-		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", exact: "4.1.0-beta.20")
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", exact: "4.1.0-beta.21")
 	],
 	targets: [
 		.target(name: "GXTwitterWrapper",
@@ -21,8 +21,8 @@ let package = Package(
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXTwitter",
-			url: "https://pkgs.genexus.dev/iOS/beta/GXTwitter-4.1.0-beta.20.xcframework.zip",
-			checksum: "29513a8dd448f6380471cecccc827b08e1a0214f373b5384ba1a1eafb1227281"
+			url: "https://pkgs.genexus.dev/iOS/beta/GXTwitter-4.1.0-beta.21.xcframework.zip",
+			checksum: "c4f4d13e05da55711398f31d252e6b62345aad077b4be4f478f068a1356934ab"
 		)
 	]
 )
